fa7c201f026bc5524462828591d638b29169de68
Luis A. Gonzalez <72740721+loconous@users.noreply.github.com>
2026-01-10 22:09:29 -0600

    app structure

---
+++ cmd/web/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet", showSnippet)
+	mux.HandleFunc("/snippet/create", createSnippet)
+
+	log.Println("Starting server on :4000")
+	if err := http.ListenAndServe(":4000", mux); err != nil {
+		log.Fatal(err)
+	}
+}
