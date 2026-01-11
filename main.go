44c32224eddffa80d174f4c539bcc812c41e1f71
Luis A. Gonzalez <72740721+loconous@users.noreply.github.com>
2026-01-10 21:53:39 -0600

    url query strings

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 // Define a home handler function which writes a byte slice containing
@@ -25,10 +27,19 @@ func home(w http.ResponseWriter, r *http.Request) {
 
 // Add a showSnippet handle function
 func showSnippet(w http.ResponseWriter, r *http.Request) {
-	_, err := w.Write([]byte("Display a specific snippet..."))
-	if err != nil {
+	// Extract the value of the id parameter from the query string and try to
+	// convert it to an integer using the strconv.Atoi() function. If it can't
+	// be converted to an integer, or the value is less than 1, we return a 404
+	// not found response.
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
 		return
 	}
+
+	// Use the fmt.Fprintf() function to interpolate the id value with our response
+	// and write it to the http.ResponseWriter.
+	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
 }
 
 // Add a createSnippet handle function
@@ -42,8 +53,9 @@ func createSnippet(w http.ResponseWriter, r *http.Request) {
 		// response header map. The first parameter is the header name, and
 		// the second parameter is the header value.
 		w.Header().Set("Allow", "POST")
-		w.WriteHeader(405)
-		w.Write([]byte("Method Not Allowed"))
+		// Use the http.Error() function to send a 405 status code and "Method Not
+		// Allowed" string as the response body.
+		http.Error(w, "Method Not Allowed", 405)
 		return
 	}
 	_, err := w.Write([]byte("Create a new snippet..."))
